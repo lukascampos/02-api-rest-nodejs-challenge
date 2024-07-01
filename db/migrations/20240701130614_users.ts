3f31964f82917825158897783e687c85d2fdf11f
lukascampos <lukas.campos@aluno.ifsp.edu.br>
2024-07-01 10:19:55 -0300

    migrations and database

---
+++ db/migrations/20240701130614_users.ts
@@ -0,0 +1,14 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary()
+    table.string('session_id').notNullable().unique()
+    table.string('name').notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('users')
+}
