c5fc704f7df58dbfea8a96f5a3016638c6eea246
lukascampos <lukas.campos@aluno.ifsp.edu.br>
2024-07-01 14:50:09 -0300

    get metrics route created - finish challenge

--- src/routes/meals/meals.ts
+++ src/routes/meals/meals.ts
@@ -141,4 +141,54 @@ export async function mealsRoutes(app: FastifyInstance) {
       return rep.status(204).send()
     },
   )
+
+  app.get(
+    '/metrics',
+    { preHandler: checkSessionIdExists },
+    async (req, rep) => {
+      const { sessionId } = req.cookies
+
+      const { id } = await knex('users')
+        .where({ session_id: sessionId })
+        .first()
+
+      const totalMealsOnDiet = await knex('meals')
+        .where({ user_id: id, is_on_diet: true })
+        .count('id', { as: 'total' })
+        .first()
+
+      const totalMealsOffDiet = await knex('meals')
+        .where({ user_id: id, is_on_diet: false })
+        .count('id', { as: 'total' })
+        .first()
+
+      const totalMeals = await knex('meals')
+        .where({ user_id: id })
+        .orderBy('created_at', 'desc')
+
+      const { bestOnDietSequence } = totalMeals.reduce(
+        (acc, meal) => {
+          if (meal.is_on_diet) {
+            acc.currentSequence += 1
+          } else {
+            acc.currentSequence = 0
+          }
+
+          if (acc.currentSequence > acc.bestOnDietSequence) {
+            acc.bestOnDietSequence = acc.currentSequence
+          }
+
+          return acc
+        },
+        { bestOnDietSequence: 0, currentSequence: 0 },
+      )
+
+      return rep.send({
+        totalMeals: totalMeals.length,
+        totalMealsOnDiet: totalMealsOnDiet?.total,
+        totalMealsOffDiet: totalMealsOffDiet?.total,
+        bestOnDietSequence,
+      })
+    },
+  )
 }
