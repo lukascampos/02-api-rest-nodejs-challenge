24ed898523e3bd022581806054a9d40a96b267c6
lukascampos <lukas.campos@aluno.ifsp.edu.br>
2024-07-01 13:39:04 -0300

    correction on Users and Meals interface

--- src/@types/db/knex.d.ts
+++ src/@types/db/knex.d.ts
@@ -3,23 +3,19 @@ import { Knex } from 'knex'
 
 declare module 'knex/types/tables' {
   export interface Users {
-    users: {
-      id: string
-      name: string
-      created_at: string
-      session_id?: string
-    }
+    id: string
+    name: string
+    created_at: string
+    session_id?: string
   }
 
   export interface Meals {
-    meals: {
-      id: string
-      user_id: string
-      name: string
-      description: string
-      is_on_diet: boolean
-      created_at: string
-      session_id?: string
-    }
+    id: string
+    user_id: string
+    name: string
+    description: string
+    is_on_diet: boolean
+    created_at: string
+    session_id?: string
   }
 }
