17590005d85aacf82aa98974af5cc3876d99cdbb
lukascampos <lukas.campos@aluno.ifsp.edu.br>
2024-07-01 10:23:43 -0300

    env port added

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import fastify from 'fastify'
 
+import { env } from './env'
+
 const app = fastify()
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('server running')
